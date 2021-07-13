4a30a8268c10e7f028945f6be9da421a5d2c983c
Danilo_Pereira <danilo_tw@hotmail.com>
2021-07-13 01:25:29 +0100

    Grid Implementation

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,61 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid';
+import Box from '../src/components/Box';
+import { AlurakutMenu, OrkutNostalgicIconSet } from "../src/lib/AlurakutCommons";
+import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSideBar(props) {
+	return (
+		<Box>
+			<img src={`https://github.com/${props.gitHubUser}.png`} style={{ borderRadius: '8px'}}/>
+		</Box>
+	)
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+	const gitHubUser = 'danilo0391';
+	const favouritePeople = [
+		'CristianMenguer',
+		'apontejaj',
+		'RogerioSobrinho',
+		'juunegreiros',
+		'omariosouto',
+		'peas'
+	]
+
+	return (
+		<>
+		<AlurakutMenu />
+		<MainGrid>
+			<div className= "profileArea" style={{ gridArea: 'profileArea' }} >
+				<ProfileSideBar gitHubUser={gitHubUser}/>
+			</div>
+			<div className= "welcomeArea" style={{ gridArea: 'welcomeArea' }} >
+				<Box>
+					<h1 className="title">
+						Welcome
+					</h1>
+
+					<OrkutNostalgicIconSet />
+				</Box>
+			</div>
+			<div className= "profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+				<ProfileRelationsBoxWrapper>
+					<h2 className="smallTitle">Comunities' People ({favouritePeople.length})</h2>
+					<ul>
+					{favouritePeople.map((item) => {
+						return (
+							<li>
+								<a href={`/users/${item}`} key={item}>
+									<img src={`https://github.com/${item}.png`} />
+									<span>{item}</span>
+								</a>
+						</li>
+							)
+					})}
+					</ul>
+				</ProfileRelationsBoxWrapper>
+			</div>
+		</MainGrid>
+		</>
+	)
 }
